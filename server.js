27bba2ac6ec4f2ffb7f502bd961f8b28cb8159e9
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-08-03 21:59:37 +0200

    13: implement a handler for `GET /` requests

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ dotenv.config({
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('<h1>hello from express!</h1>');
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
