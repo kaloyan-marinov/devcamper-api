1ee3a41acb8e943470969809fade81d8132a3fef
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-08-03 22:49:58 +0200

    15: extract *controller methods* out of `routes/bootcamps.js` (into a newly-created `controllers/bootcamps.js`)

---
+++ controllers/bootcamps.js
@@ -0,0 +1,57 @@
+/*
+Basically, the symbols defined in this file are *middleware functions*.
+
+A *middleware function* is a function,
+runs during the request-response cycle
+and, as such, has access to the `req` and `res` objects (as well as to `next`).
+*/
+
+// @desc      Get all bootcamps
+// @route     GET /api/v1/bootcamps
+// @access    Public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: 'Show all bootcamps',
+  });
+};
+
+// @desc      Get single bootcamps
+// @route     GET /api/v1/bootcamps/:id
+// @access    Public
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: `Show bootcamp ${req.params.id}`,
+  });
+};
+
+// @desc      Create new bootcamp
+// @route     POST /api/v1/bootcamp
+// @access    Private
+exports.createBootcamp = (req, res, next) => {
+  res.status(201).json({
+    success: true,
+    msg: 'Create new bootcamp',
+  });
+};
+
+// @desc      Update bootcamp
+// @route     PUT /api/v1/bootcamps/:id
+// @access    Private
+exports.updateBootcamp = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: `Update bootcamp ${req.params.id}`,
+  });
+};
+
+// @desc      Delete bootcamp
+// @route     DELETE /api/v1/bootcamps/:id
+// @access    Private
+exports.deleteBootcamp = (req, res, next) => {
+  res.status(200).json({
+    success: true,
+    msg: `Delete bootcamp ${req.params.id}`,
+  });
+};
