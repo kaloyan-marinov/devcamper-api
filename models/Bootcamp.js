3ce18d987a5d0c56180d0138a687231d160f7702
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-08-04 23:31:25 +0200

    31: implement a Mongoose hook/middleware, which geocodes the address that is submitted with a `POST /api/v1/bootcamps` request

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
 
 const BootcampSchema = new mongoose.Schema({
   name: {
@@ -37,26 +38,27 @@ const BootcampSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please add an address'],
   },
-  // location: {
-  //   // GeoJSON Point - cf. https://mongoosejs.com/docs/geojson.html
-  //   type: {
-  //     type: String,
-  //     enum: ['Point'],
-  //     required: true,
-  //   },
-  //   coordinates: {
-  //     type: [Number],
-  //     required: true,
-  //     index: '2dsphere',
-  //   },
-  //   // From the MapQuest API (aka the "GeoCoder")
-  //   formattedAddress: String,
-  //   street: String,
-  //   city: String,
-  //   state: String,
-  //   zipcode: String,
-  //   country: String,
-  // },
+  location: {
+    // GeoJSON Point - cf. https://mongoosejs.com/docs/geojson.html
+    // These are required:
+    type: {
+      type: String,
+      enum: ['Point'],
+    },
+    coordinates: {
+      type: [Number],
+      index: '2dsphere',
+    },
+    // These are not required;
+    // they come
+    // From the MapQuest API (aka the "GeoCoder"):
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
   careers: {
     // Array of strings
     type: [String],
@@ -111,6 +113,28 @@ BootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, {
     lower: true,
   });
+
+  next();
+});
+
+// Geocode && create location field
+BootcampSchema.pre('save', async function (next) {
+  const loc = await geocoder.geocode(this.address);
+
+  // Construct a GeoJSON object
+  this.location = {
+    type: 'Point',
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
+
+  // Do not save address in DB
+  this.address = undefined;
 
   next();
 });
