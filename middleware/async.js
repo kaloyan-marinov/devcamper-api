f87f942130c7944b2044d917abef4c2a0f76cad6
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-08-04 22:38:13 +0200

    28: avoid repeating the `try`/`catch` blocks in each async controller (middleware) by writing it once in a single dedicated middleware (called `asyncHandler`)

---
+++ middleware/async.js
@@ -0,0 +1,6 @@
+// Source: https://www.acuriousanimal.com/blog/20180315/express-async-middleware
+const asyncHandler = (fn) => (req, res, next) => {
+  Promise.resolve(fn(req, res, next)).catch(next);
+};
+
+module.exports = asyncHandler;
