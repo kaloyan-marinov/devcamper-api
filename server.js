729a3a057cf3809f910eb27c01a73932692a1dd2
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-08-04 12:36:27 +0200

    22: rectify the issue described in the preceding commit's message (by making the `app` use a body-parser middleware function, which is part of the `express` package itself)

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body parser (middleware that is part of `express`)
+app.use(express.json());
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
