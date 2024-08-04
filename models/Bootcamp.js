9d3e95ad170857423ad526173da7920f4eee5526
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-08-04 12:21:43 +0200

    21: add `models/Bootcamp.js` which implements a `BootcampSchema` and exports a `mongoose.model('Bootcamp', BootcampSchema)`

---
+++ models/Bootcamp.js
@@ -0,0 +1,105 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxlength: [50, 'Name cannot be more than 50 characters'],
+  },
+  slug: String, // This is a URL-friendly version of the `name`. (E.g. "Devcentral Bootcamp" -> "devcentral-bootcamp")
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    maxlength: [500, 'Description cannot be more than 500 characters'],
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+      'Please use a valid URL with HTTP or HTTPS',
+    ], // At the time when the video was recorded, this regex came from: https://stackoverflow.com/a/3809435
+  },
+  phone: {
+    type: String,
+    maxlength: [28, 'Phone number cannot be longer than 20 characters'],
+  },
+  email: {
+    type: String,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email',
+    ], // At the time when the video was recorded, this regex came from: https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
+  },
+  address: {
+    type: String,
+    required: [true, 'Please add an address'],
+  },
+  location: {
+    // GeoJSON Point - cf. https://mongoosejs.com/docs/geojson.html
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+      index: '2dsphere',
+    },
+    // From the MapQuest API (aka the "GeoCoder")
+    formattedAddress: String,
+    street: String,
+    city: String,
+    state: String,
+    zipcode: String,
+    country: String,
+  },
+  careers: {
+    // Array of strings
+    type: [String],
+    required: true,
+    enum: [
+      'Web Development',
+      'Mobile Development',
+      'UI/UX',
+      'Data Science',
+      'Business',
+      'Other',
+    ],
+  },
+  averageRating: {
+    type: Number,
+    min: [1, 'Rating must be at least 1'],
+    max: [10, 'Rating cannot be more than 10'],
+  },
+  averageCost: Number,
+  photo: {
+    type: String,
+    default: 'no-photo.jpg',
+  },
+  housing: {
+    type: Boolean,
+    default: false,
+  },
+  jobAssistance: {
+    type: Boolean,
+    default: false,
+  },
+  jobGuarantee: {
+    type: Boolean,
+    default: false,
+  },
+  acceptGi: {
+    // Do they accept a GI bill?
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
