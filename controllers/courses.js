2c82d8f8b96919d338f7c8b77942708f5bbee246
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-08-07 22:53:12 +0200

    40: implement handling of `POST /api/v1/bootcamps/:bootcampId/courses` requests

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
 
 // @desc    Get courses
 // @route   GET /api/v1/courses
@@ -45,6 +46,30 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
   }
 
   res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc    Add course
+// @route   POST /api/v1/bootcamps/:bootcampId/courses
+// @access  Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    const errorResponse = new ErrorResponse(
+      `No bootcamp with the ID of ${req.params.bootcampId}`,
+      404
+    );
+    return next(errorResponse);
+  }
+
+  const course = await Course.create(req.body);
+
+  res.status(201).json({
     success: true,
     data: course,
   });
