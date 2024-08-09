5c7b7e97a2074565ab53804e379a10dabb0ee1c7
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-08-09 22:48:09 +0200

    45: create `models/User.js`

---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: true,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email',
+    ], // At the time when the video was recorded, this regex came from: https://stackoverflow.com/questions/46155/how-can-i-validate-an-email-address-in-javascript
+  },
+  role: {
+    type: String,
+    enum: ['user', 'publisher'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minLength: 6,
+    select: false, // When HTTP clients get a User through the API, the server isn't going to send the password in its HTTP response.
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
