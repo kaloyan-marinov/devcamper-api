1c114ee0e72eec201a9c37646b045b4b85127a31
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-08-10 18:16:26 +0200

    57: utilize `sendEmail` in the `forgotPassword` controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/User');
 
 // @desc    Register user
@@ -94,10 +95,37 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     validateBeforeSave: false,
   });
 
-  res.status(200).json({
-    success: false,
-    data: user,
-  });
+  // Create reset URL
+  const resetUrl = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/resetpassword/${resetToken}`;
+
+  const message = `You are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to: \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message,
+    });
+
+    res.status(200).json({
+      success: true,
+      data: 'Email sent',
+    });
+  } catch (err) {
+    console.error(err);
+
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save({
+      validateBeforeSave: false,
+    });
+
+    // Generate an HTTP response with a status code of 500.
+    const errorResponse = new ErrorResponse('Email could not be sent', 500);
+    return next(errorResponse);
+  }
 });
 
 // const HOURS_PER_DAY = 24;
