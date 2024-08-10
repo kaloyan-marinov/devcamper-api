9ef2d3374ca7b8c11a269ced9ccfd67a5c0cae0d
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-08-10 14:11:13 +0200

    53: edit `createBootcamp` into making sure that the `Bootcamp` has its `user` field populated + ensuring that only an `admin` can create more than 1 `Bootcamp` resource

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -46,6 +46,25 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route     POST /api/v1/bootcamp
 // @access    Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  // Add user to req.body
+  req.body.user = req.user.id;
+
+  // Check for published bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({
+    user: req.user.id,
+  });
+
+  // If the user is not an admin,
+  // they can only add one bootcamp.
+  if (publishedBootcamp && req.user.role !== 'admin') {
+    const errorResponse = new ErrorResponse(
+      `The user with ID ${req.user.id} has already published a bootcamp`,
+      400
+    );
+
+    next(errorResponse);
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
 
   res.status(201).json({
