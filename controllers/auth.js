b03cd0fcfa3a90ff846b7d54ae7be0b5a5a60373
Kaloyan Marinov <happy-2024-01-02-16-53@protonmail.com>
2024-08-10 23:41:12 +0200

    58: implement handling of `PUT /api/v1/auth/resetpassword/:resettoken` requests

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const crypto = require('crypto');
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const sendEmail = require('../utils/sendEmail');
@@ -126,6 +127,39 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
     const errorResponse = new ErrorResponse('Email could not be sent', 500);
     return next(errorResponse);
   }
+});
+
+// @desc      Reset password
+// @route     PUT /api/v1/auth/resetpassword/:resettoken
+// @access    Public
+exports.resetPassword = asyncHandler(async (req, res, next) => {
+  // Get hashed token
+  const resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(req.params.resettoken)
+    .digest('hex');
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: {
+      $gt: Date.now(),
+    },
+  });
+
+  if (!user) {
+    const errorResponse = new ErrorResponse('Invalid token', 401);
+
+    return next(errorResponse);
+  }
+
+  // Set new password
+  // (which will get encrypted by the relevant Mongoose hook/middleware)
+  user.password = req.body.password;
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpire = undefined;
+  await user.save();
+
+  sendTokenResponse(user, 200, res);
 });
 
 // const HOURS_PER_DAY = 24;
